dafb7e91633350183db76d68ba6b2449bfd4f195
Gilbert-Silva <gilbertif1995@outlook.com>
2026-06-18 14:24:27 -0300

    Json

---
+++ Aula_08/ex01.py
@@ -0,0 +1,15 @@
+# 0 1 2 3
+# cada linha é um input
+
+s = input()   # "0 1 2 3"
+x = s.split() # ["0", "1", "2", "3"]
+a = int(x[0])
+b = int(x[1])
+c = int(x[2])
+d = int(x[3])
+print(a + b + c + d)
+
+a, b, c, d = map(int, input().split())
+print(a + b + c + d)
+
+

ec60a6cf6e46ff46ca04b74ec15c6f72554ef6cc
Gilbert-Silva <gilbertif1995@outlook.com>
2026-08-11 17:24:35 +0000

    Streamlit

---
+++ Aula_10/index.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from retanguloUI import RetanguloUI 
+
+#st.header("POO em Python com Streamlit")
+#st.write("InfoWeb Turma 2026 - 2º Ano")
+#if st.button("Clique Aqui"):
+#    st.write("Ben-vindo(a) ao Streamlit")
+
+RetanguloUI.main()
